f2c1956c78d6977509429200adcb2c7307ccac9e
philcurtis422 <philcurtis422@gmail.com>
2024-07-29 10:36:10 -0400

    finished

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,22 @@
-const {Client} = require('pg');
-const client = new Client({
+const {Pool} = require('pg');
+const is_prod = process.env.NODE_ENV === 'production';
+
+
+const localConnection = {
 	user: 'postgres',
 	password: 'pass',
 	database: 'student_app_db'
-});
+};
+
+const renderConnection = {
+	connectionString: process.env.DB_URL,
+	ssl: {
+		rejectUnauthorized: false
+	}
+}
+
+const connectObj = is_prod ? renderConnection : localConnection;
+
+const client = new Pool(connectObj);
 
 module.exports = client;
